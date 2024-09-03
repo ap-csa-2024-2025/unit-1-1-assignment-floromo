1414dd69d864cc477c7155cd8f5f767ebce99f9d
floromo <flormmmp@gmail.com>
2024-09-03 10:05:49 -0700

    followed along in class

---
+++ Printing.java
@@ -0,0 +1,15 @@
+public class Printing {
+    public static void main (String[] args) {
+        // --------- VARIABLES ---------
+        // int, double, boolean, String
+
+        int myAge = 16;     // int = integers = whole #s
+        double price = 0.01;    // double = decimals
+
+        boolean isbad;      // declaration
+        isbad = true;       // initialization
+
+        String name = "Sofia Nguyen";
+        System.out.println("Hello, my name is " + name + ".");
+    }
+}
